bea14eaea54e1fe31e4a804f3451e2a61e28c80f
rymuelle <33radar@gmail.com>
2017-02-23 09:59:45 -0600

    Create ZprimeToll_M3000_13TeV_pythia8_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_RAW2DIGI_RECO.py
    Z' config file.

---
+++ SingleMuonGun/test/ZprimeToll_M3000_13TeV_pythia8_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_RAW2DIGI_RECO.py
@@ -0,0 +1,197 @@
+# Auto generated configuration file
+# using:
+# Revision: 1.19
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v
+# with command line options: ZprimeToll_M3000_13TeV_pythia8_cff.py --mc -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT,RAW2DIGI,RECO -n 10 --no_exec --conditions=auto:run2_mc --eventcontent RECO --pileup=NoPileUp --era Run2_25ns
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('HLT',eras.Run2_25ns)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.Geometry.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic50ns13TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('HLTrigger.Configuration.HLT_GRun_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('ZprimeToll_M3000_13TeV_pythia8_cff.py nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+# IMPORTANT! Define output event content ***************************************
+fullRECO = False
+if (fullRECO == True):
+  print "WARNING! Output format: FULL RECO"
+  outputEventContent = process.RECOEventContent.outputCommands
+  oututFileName = cms.untracked.string('singleMuonGun_FullRECO.root')
+else:
+  print "WARNING! Output format: REDUCED RECO aka ALCARECO"
+    oututFileName = cms.untracked.string('singleMuonGun_ReducedRECO.root')
+    outputEventContent = cms.untracked.vstring('drop *',
+      # Keep all type of muons
+      'keep *_muons_*_*',
+#      'keep *_muonsFromCosmics_*_*',
+#      'keep *_muonsFromCosmics1Leg_*_*',
+      # Keep general tracks
+      'keep *_generalTracks_*_*',
+      # Keep muon tracks
+      'keep *_globalMuons_*_*',
+      'keep *_standAloneMuons_*_*',
+#      'keep *_refittedStandAloneMuons_*_*',
+#      'keep *_displacedStandAloneMuons_*_*',
+#      'keep *_standAloneSETMuons_*_*',
+#`      'keep *_globalSETMuons_*_*',
+#      'keep *_tevMuons_*_*',
+#      'keep *_cosmicMuons_*_*',
+#      'keep *_cosmicMuons1Leg_*_*',
+#      'keep *_globalCosmicMuons_*_*',
+#      'keep *_globalCosmicMuons1Leg_*_*',
+      # Keep all muon hits information
+      'keep *_muonCSCDigis_*_*',
+      'keep *_muonDTDigis_*_*',
+      'keep *_muonRPCDigis_*_*',
+      'keep *_dt1DRecHits_*_*',
+      'keep *_dt2DSegments_*_*',
+      'keep *_dt4DSegments_*_*',
+      'keep *_csc2DRecHits_*_*',
+      'keep *_cscSegments_*_*',
+      'keep *_rpcRecHits_*_*',
+      # Keep tracker hits information
+      'keep SiPixelClusteredmNewDetSetVector_siPixelClusters_*_*',
+      'keep SiStripClusteredmNewDetSetVector_siStripClusters_*_*',
+      # Keep trigger information
+      'keep *_TriggerResults_*_*',
+      'keep L1AcceptBunchCrossings_*_*_*',
+      'keep L1GlobalTriggerReadoutRecord_gtDigis_*_*',
+      # Keep DCS information
+      'keep DcsStatuss_scalersRawToDigi_*_*',
+      # Keep beam spot and vertices
+      'keep *_offlineBeamSpot_*_*',
+      'keep *_offlinePrimaryVertices_*_*',
+      # Keep generator level information
+      'keep *_genParticles_*_*',
+      'keep *_generator_*_*',
+      'drop edmHepMCProduct_generator_*_*',
+    )
+# ******************************************************************************
+# Output definition
+
+process.RECOoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO'),
+        filterName = cms.untracked.string('')
+    ),
+        eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    #fileName = cms.untracked.string('singleMuonGun_V2_cfi_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_RAW2DIGI_RECO.root'),
+    fileName = oututFileName,
+    outputCommands = outputEventContent,
+    #outputCommands = process.RECOEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+
+process.generator = cms.EDFilter("Pythia8GeneratorFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring('pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters'),
+        processParameters = cms.vstring('NewGaugeBoson:ffbar2gmZZprime = on',
+            'Zprime:gmZmode = 3',
+            '32:m0 = 3000',
+            '32:onMode = off',
+            '32:onIfAny = 13'),
+        pythia8CUEP8M1Settings = cms.vstring('Tune:pp 14',
+            'Tune:ee 7',
+            'MultipartonInteractions:pT0Ref=2.4024',
+            'MultipartonInteractions:ecmPow=0.25208',
+            'MultipartonInteractions:expPow=1.6'),
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2',
+            'Main:timesAllowErrors = 10000',
+            'Check:epTolErr = 0.01',
+            'Beams:setProductionScalesFromLHEF = off',
+            'SLHA:keepSM = on',
+            'SLHA:minMassSM = 1000.',
+            'ParticleDecays:limitTau0 = on',
+            'ParticleDecays:tau0Max = 10',
+            'ParticleDecays:allowPhotonRadiation = on')
+    ),
+    comEnergy = cms.double(13000.0),
+    crossSection = cms.untracked.double(0.013),
+    filterEfficiency = cms.untracked.double(1),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(1)
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOoutput_step = cms.EndPath(process.RECOoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.digitisation_step,process.L1simulation_step,process.digi2raw_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.raw2digi_step,process.reconstruction_step,process.endjob_step,process.RECOoutput_step])
+# filter all path with the production filter sequence
+for path in process.paths:
+    getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from HLTrigger.Configuration.customizeHLTforMC
+from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforFullSim
+
+#call to customisation function customizeHLTforFullSim imported from HLTrigger.Configuration.customizeHLTforMC
+process = customizeHLTforFullSim(process)
+
+SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",ignoreTotal = cms.untracked.int32(1) )
+# End of customisation functions
